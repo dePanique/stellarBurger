cdb49015e02b32840b729b04cd26d5487ac6c6a7
Oleg Popov <de.simpl@yandex.ru>
2022-05-18 23:50:37 +0300

    refactor: divide list item in separate component

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,31 +1,40 @@
 import styles from './app-header.module.css'
-import { Logo, Typography, Icons, Box, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import HeaderListItem from '../header-list-item/header-list-item'
 
 
 const AppHeader = () => {
+
   return(
     <header className={styles.header}>
       <nav>
         <ul className={styles.row + ' pt-4 pb-4'}>
-          <li className={styles.element + ' pl-5 pr-5'}>
-            <BurgerIcon type='primary'/>
-            <a href="#" className={styles.link + ' text text_type_main-default text_color_primary ml-2'}>Конструктор</a>
-          </li>
-          <li className={styles.element + ' pl-5 pr-5 ml-2'}>
-            <ListIcon type='secondary'/>
-            <a href="#" className={styles.link + ' text text_type_main-default text_color_inactive ml-2'}>Лента заказов</a>
-          </li>
-          <li className={styles.element}>
-            <Logo />
-          </li>
-          <li className={styles.element + ' pl-5 pr-5'}>
-            <ProfileIcon type='secondary'/>
-            <a href="#" className={styles.link + ' text text_type_main-default text_color_inactive ml-2'}>Личный кабинет</a>
-          </li>
+
+          <HeaderListItem
+            icon={<BurgerIcon type='primary'/>}
+            spanText='Конструктор'
+            isActive={true}
+            />
+
+          <HeaderListItem
+            icon={<ListIcon type='secondary'/>}
+            spanText='Лента заказов'
+            itemStyle={' ml-2'}
+            />
+
+          <HeaderListItem
+            logo={<Logo />}
+            />
+
+          <HeaderListItem
+            icon={<ProfileIcon type='secondary'/>}
+            spanText='Личный кабинет'
+            />
+
         </ul>
       </nav>
     </header>
   )
 }
 
-export default AppHeader;
+export default AppHeader
---
+++ src/components/header-list-item/header-list-item.jsx
@@ -0,0 +1,19 @@
+import styles from './header-list-item.module.css'
+
+const HeaderListItem = (props) => {
+  return (
+    props.logo ?
+    <li className={styles.logo}>
+      <a href={(props.href || "#")} className={styles.link}>{props.logo}</a>
+    </li>
+    :
+    <li className={styles.element + ' pl-5 pt-4 pr-5 pb-4'}>
+      <a href={(props.href || "#")} className={styles.link + ' ml-2 '}>
+        {props.icon}
+        <span className={(props.isActive && ' text_color_primary ' || 'text_color_inactive ') + ' ml-2 text text_type_main-default'}>{props.spanText}</span>
+      </a>
+    </li>
+  )
+}
+
+export default HeaderListItem
