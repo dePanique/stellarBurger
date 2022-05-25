12dd3a9229fbd4a2c7a920b434cd999b6b618b09
Oleg Popov <de.simpl@yandex.ru>
2022-05-25 23:41:47 +0300

    feat/ add scroll-behavior for burger-ingridient's collection

--- src/components/burger-ingredients/burger-ingridients.jsx
+++ src/components/burger-ingredients/burger-ingridients.jsx
@@ -11,25 +11,31 @@ const BurgerIngridients = ({ data }) => {
   return(
     <section className={styles.burgerIngridients + ' mr-5'}>
       <h2 className={styles.title + ' text text_type_main-large mt-10 mb-5'}>Соберите бургер</h2>
-      
+
       <div className={styles.tabRow + ' mb-10'}>
-        <Tab value="Булки" active={tab === 'Булки'} onClick={setTab}>
-          Булки
-        </Tab>
-        <Tab value="Соусы" active={tab === 'Соусы'} onClick={setTab}>
-          Соусы
-        </Tab>
-        <Tab value="Начинки" active={tab === 'Начинки'} onClick={setTab}>
-          Начинки
-        </Tab>
+        <a href='#bun' className={styles.link + ' text text_type_main-default'}>
+          <Tab value="Булки" active={tab === 'Булки'} onClick={setTab}>
+            Булки
+          </Tab>
+        </a>
+        <a href="#sauces" className={styles.link + ' text text_type_main-default'}>
+          <Tab value="Соусы" active={tab === 'Соусы'} onClick={setTab}>
+            Соусы
+          </Tab>
+        </a>
+        <a href='#main' className={styles.link + ' text text_type_main-default'}>
+          <Tab value="Начинки" active={tab === 'Начинки'} onClick={setTab}>
+            Начинки
+          </Tab>
+        </a>
       </div>
 
       <div className={styles.collection}>
-        <p className={styles.ingridientsType + ' text text_type_main-medium'}>Булки</p>
+        <p id='bun' className={styles.ingridientsType + ' text text_type_main-medium'}>Булки</p>
         {data.map((element) => element.type === 'bun' && <Ingridient key={element._id} {...element}/>)}
-        <p className={styles.ingridientsType + ' text text_type_main-medium mt-10'}>Соусы</p>
+        <p id='sauces' className={styles.ingridientsType + ' text text_type_main-medium mt-10'}>Соусы</p>
         {data.map((element) => element.type === 'sauce' && <Ingridient key={element._id} {...element}/>)}
-        <p className={styles.ingridientsType + ' text text_type_main-medium mt-10'}>Начинки</p>
+        <p id='main' className={styles.ingridientsType + ' text text_type_main-medium mt-10'}>Начинки</p>
         {data.map((element) => element.type === 'main' && <Ingridient key={element._id} {...element}/>)}
       </div>
     </section>
