77a3ab7be0d0806043e2f36196855678d8a52aac
Oleg Popov <de.simpl@yandex.ru>
2022-06-02 19:48:31 +0300

    fix: fix parentheses ambiguity in header-listitem

--- src/components/header-list-item/header-list-item.jsx
+++ src/components/header-list-item/header-list-item.jsx
@@ -11,7 +11,7 @@ const HeaderListItem = ({ ...props }) => {
     <li className={styles.element + ' pl-5 pt-4 pr-5 pb-4' + (props.itemStyle ? props.itemStyle : '')}>
       <a href={(props.href || "#")} className={styles.link + ' ml-2 '}>
         {props.icon}
-        <span className={(props.isActive && ' text_color_primary ' || 'text_color_inactive ') + ' ml-2 text text_type_main-default'}>{props.spanText}</span>
+        <span className={((props.isActive && ' text_color_primary ') || 'text_color_inactive ') + ' ml-2 text text_type_main-default'}>{props.spanText}</span>
       </a>
     </li>
   )
