5b0b74a4ae94752b6536953ccf13fd6a6c4dae7c
Oleg Popov <de.simpl@yandex.ru>
2022-07-08 17:01:41 +0300

    feat: set tab on scroll in ingredients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import styles from "./burger-ingredients.module.css";
 import IngredientsCollection from "./../ingredients-collection/ingredients-collection";
 import Modal from "../modal/modal";
 import IngredientDetails from "../ingredient-details/ingredient-details";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useEffect } from "react";
 
 const BurgerIngredients = () => {
-  const [tab, setTab] = useState("Булки");
   const [modal, setModal] = useState(false);
-  const handle = () => {
-    console.log(3)
+  const [activeTab, setActiveTab] = useState('Булки');
+  const [currentScrollPos, setCurrentScrollPos] = useState(0);
+  const [positionForActivation, setPositionForActivation] = useState([]);
+
+  useEffect(() => {
+    let offsets = ['#bun', '#sauce', '#main'].map((el) => document.querySelector(el).offsetTop )
+
+    setPositionForActivation(offsets.map((_, index) => {
+      if (index === 0) {
+        return 0;
+      }
+      else if (index === 1) {
+        return offsets[1] - offsets[0] - 40;
+      }
+      else {
+        return offsets[2] - offsets[0] - 40;
+      }
+    }))
+  }, [])
+
+  const setCurrentTab = (e) => {
+    setCurrentScrollPos(e.target.scrollTop)
+    if (currentScrollPos < positionForActivation[1]) {
+      setActiveTab('Булки')
+    } else if (currentScrollPos > positionForActivation[1] && currentScrollPos < positionForActivation[2]) {
+      setActiveTab('Соусы')
+    } else if (currentScrollPos > positionForActivation[2]) {
+      setActiveTab('Начинки')
+    }
   }
 
   return (
@@ -20,7 +47,7 @@ const BurgerIngredients = () => {
 
       <div className={styles.tabRow + " mb-10"}>
         <a href="#bun" className={styles.link + " text text_type_main-default"}>
-          <Tab value="Булки" active={tab === "Булки"} onClick={setTab}>
+          <Tab value="Булки" active={activeTab === "Булки"} onClick={setActiveTab}>
             Булки
           </Tab>
         </a>
@@ -28,7 +55,7 @@ const BurgerIngredients = () => {
           href="#sauce"
           className={styles.link + " text text_type_main-default"}
         >
-          <Tab value="Соусы" active={tab === "Соусы"} onClick={setTab}>
+          <Tab value="Соусы" active={activeTab === "Соусы"} onClick={setActiveTab}>
             Соусы
           </Tab>
         </a>
@@ -36,13 +63,13 @@ const BurgerIngredients = () => {
           href="#main"
           className={styles.link + " text text_type_main-default"}
         >
-          <Tab value="Начинки" active={tab === "Начинки"} onClick={setTab} >
+          <Tab value="Начинки" active={activeTab === "Начинки"} onClick={setActiveTab} >
             Начинки
           </Tab>
         </a>
       </div>
 
-      <div className={styles.collection}>
+      <div className={styles.collection} onScroll={e => setCurrentTab(e)}>
         <IngredientsCollection
           type={"bun"}
           setModal={setModal}
@@ -56,7 +83,6 @@ const BurgerIngredients = () => {
         <IngredientsCollection
           type={"main"}
           setModal={setModal}
-          onFocus={() => this.handle()}
         />
       </div>
 
