8bbd33e4b091323d9783cd135a31614a7e0f6424
Oleg Popov <de.simpl@yandex.ru>
2022-07-10 19:04:32 +0300

    refac: delete createStore

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './components/app/app'
-// import { compose, createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { rootReducer } from './components/services/reducers';
-
-// const composeEnhancers =
-//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-//     : compose;
-
-// const enhancer = composeEnhancers(applyMiddleware(thunk));
-// const store = createStore(rootReducer, enhancer);
 
 const store = configureStore({
   reducer: rootReducer,
