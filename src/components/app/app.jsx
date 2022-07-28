b84a392d416858fb88cf292e6d795c97fa89437f
Oleg Popov <de.simpl@yandex.ru>
2022-07-28 12:47:32 +0300

    refactor: mkdir pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,34 +1,16 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import React from "react";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import Main from "../main/main";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { getIngredients } from "../../services/actions/app";
+import { HomePage } from "../../pages"
 
 function App() {
-  const dispatch = useDispatch();
-  const isDataAvailable = useSelector((store) => store.appStore.success);
-
-  useEffect(() => {
-    dispatch(getIngredients());
-  }, []);
 
   return (
     <div className={styles.app}>
       <AppHeader />
       <Main>
-        {isDataAvailable && (
-          <React.Fragment>
-            <DndProvider backend={HTML5Backend}>
-              <BurgerIngredients />
-              <BurgerConstructor />
-            </DndProvider>
-          </React.Fragment>
-        )}
+        <HomePage />
       </Main>
     </div>
   );
---
+++ src/pages/home-page.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getIngredients } from "../services/actions/app";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import BurgerIngredients from "../components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../components/burger-constructor/burger-constructor";
+
+export const HomePage = () => {
+  const dispatch = useDispatch();
+  const isDataAvailable = useSelector((store) => store.appStore.success);
+
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, []);
+
+  return (
+    isDataAvailable && (
+      <React.Fragment>
+        <DndProvider backend={HTML5Backend}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </DndProvider>
+      </React.Fragment>
+    )
+  )
+}
