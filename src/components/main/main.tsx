31e2f3863fc48dc27eeb77f1d790be4a681d894f
Oleg Popov <de.simpl@yandex.ru>
2022-09-14 15:47:23 +0300

    refac: refac Main

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,17 +1,17 @@
-import PropTypes from "prop-types";
+import { FC, ReactNode } from "react";
 import styles from "./main.module.css";
 
-const Main = (props) => {
-  
+interface IMain {
+  children: ReactNode
+}
+
+const Main: FC<IMain> = (props) => {
+
   return (
     <main className={styles.main}>
       {props.children}
     </main>
   )
-};
-
-Main.propTypes = {
-  props: PropTypes.element,
 };
 
 export default Main;
