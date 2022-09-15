fc283baafcd385e65b9e06f0dc16978030bedecb
Oleg Popov <de.simpl@yandex.ru>
2022-09-15 11:15:47 +0300

    refac: refac home-page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,14 +1,16 @@
-import { useSelector } from "react-redux";
+import { FC } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import BurgerIngredients from "../components/burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../components/burger-constructor/burger-constructor";
 import Main from "../components/main/main";
+import { appUseSelector } from "../utils/hooks";
 
 
-export const HomePage = () => {
-  const isDataAvailable = useSelector((store) => store.appStore.success);
+export const HomePage: FC = () => {
+
+  const isDataAvailable = appUseSelector((store) => store.appStore.success);
 
   return (
     isDataAvailable && (
