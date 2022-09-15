9cb7b05164ed707440afe66862321c35a1cf5adb
Oleg Popov <de.simpl@yandex.ru>
2022-09-15 11:20:45 +0300

    refac: refac ingredient=page

--- src/pages/ingredient-page.tsx
+++ src/pages/ingredient-page.tsx
@@ -1,17 +1,20 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { FC } from "react";
 import IngredientDetails from "../components/ingredient-details/ingredient-details"
 import Main from "../components/main/main";
 import { SET_CURRENT_INGREDIENT } from "../services/actions/burger-ingredients";
 import styles from './ingredient-page.module.css'
 import { useParams } from 'react-router-dom'
+import { appUseDispatch, appUseSelector } from "../utils/hooks";
+import { TIngredient } from "../utils/type";
 
-export const IngredientPage = () => {
-  const dispatch = useDispatch();
+export const IngredientPage: FC = () => {
 
-  const { id } = useParams();
+  const dispatch = appUseDispatch();
 
-  const { data } = useSelector((store) => store.appStore);
+  const { id }: { id: string } = useParams();
+
+  const { data }: { data: TIngredient[] } = appUseSelector((store) => store.appStore);
   const ingredient = data.find(element => element._id === id);
 
   //Не трогай т.к. без него react cannot update a component
@@ -21,15 +24,19 @@ export const IngredientPage = () => {
       dispatch({
         type: SET_CURRENT_INGREDIENT,
         payload: ingredient
-      })
+      });
     }
-  }, [ingredient])
+  }, [ingredient]);
 
   return (
-    ingredient && <Main>
-      <div className={styles.frame}>
-        <IngredientDetails />
-      </div>
-    </Main>
-  )
+    ingredient ? (
+      <Main>
+        <div className={styles.frame}>
+          <IngredientDetails />
+        </div>
+      </Main>
+    ) : (
+      null
+    )
+  );
 }
